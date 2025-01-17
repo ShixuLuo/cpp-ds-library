40c0371f9e0703ad8fea5b4e22472ab95dd16187
Shixu Luo <luo.shix@northeastern.edu>
2025-01-17 17:54:53 -0500

    feat: introduce class LinkedList and class methods
    Methods including push_back, push_front, pop_front, get_size, isEmpty.

--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -1,12 +1,19 @@
 #include "linkedlist.h"
 
 #include <stdexcept>
-#include <iostream>
+#include <functional>
 
 LinkNode::LinkNode(int value) : value(value), next(nullptr) {}
 
+/**
+ * @test
+ * @SUBCASE create
+ * INPUT LinkedList llist();
+ * EXPECT llist.head == nullptr;
+ */
 LinkedList::LinkedList() {
     head = nullptr;
+    size = 0;
 }
 
 LinkedList::~LinkedList() {
@@ -19,14 +26,88 @@ LinkedList::~LinkedList() {
     head = nullptr;
 }
 
+// Private
+
+bool LinkedList::isInRange(int index) const {
+    return (index >= 0 && index < size);
+}
+
+void LinkedList::ensureInRange(int index) const {
+    if (!isInRange(index)) {
+        throw std::out_of_range("Out of range");
+    }
+}
+
+LinkNode* LinkedList::getNode(int index) const {
+    LinkNode* current = head;
+    int i = 0;
+    while (i != index) {
+        if (current->next == nullptr) {
+            return current;
+        }
+        current = current->next;
+        ++i;
+    }
+    return current;
+}
+
+// Public
+
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1
+ * INPUT set(2, 42);
+ * EXPECT 5->4->42->2->0
+ */
+void LinkedList::set(int index, int value) {
+    ensureInRange(index);
+
+    LinkNode* node = getNode(index);
+    node->value = value;
+}
+
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1
+ * INPUT llist.get(2);
+ * EXPECT 3;
+ *
+ * CASE 2: exception
+ * INPUT llist.get(-1);
+ * EXPECT std::out_of_range;
+ */
+int LinkedList::get(int index) const {
+    ensureInRange(index);
+
+    LinkNode* node = getNode(index);
+    return node->value;
+}
+
+/**
+ * @notest
+ */
 void LinkedList::push_front(int value) {
     LinkNode* temp = head;
     head = new LinkNode(value);
     head->next = temp;
+    size = size + 1;
 }
 
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1;
+ * INPUT pop_front()
+ * EXPECT 4->3->2->1, size=4;
+ *
+ * CASE 2: exception
+ * INPUT pop_front()
+ * EXPECT std::runtime_error
+ */
 int LinkedList::pop_front() {
-    if (head == nullptr) {
+    if (isEmpty()) {
         throw std::runtime_error("Attempt to delete an element from "
                                  "an empty linked list");
     }
@@ -34,26 +115,68 @@ int LinkedList::pop_front() {
     LinkNode* oldHead = head;
     head = head->next;
     delete oldHead;
+    size = size - 1;
     return value;
 }
 
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1;
+ * INPUT push_back(42);
+ * EXPECT 5->4->3->2->1->42, size=6;
+ */
 void LinkedList::push_back(int value) {
-    if (head == nullptr) {
+    if (isEmpty()) {
         head = new LinkNode(value);
     } else {
-        LinkNode* current = head;
-        while (current->next != nullptr) {
-            current = current->next;
-        }
-        current->next = new LinkNode(value);
+        LinkNode* tail = getNode(-1);
+        tail->next = new LinkNode(value);
+    }
+    size = size + 1;
+}
+
+int LinkedList::pop_back() {
+    throw std::runtime_error("Function not implemented");
+    return -1;
+}
+
+void LinkedList::insert(int index, int value) {
+    throw std::runtime_error("Function not implemented");
+}
+
+void LinkedList::remove(int index) {
+    throw std::runtime_error("Function not implemented");
+}
+
+int LinkedList::find(int value) const {
+    throw std::runtime_error("Function not implemented");
+    return -1;
+}
+
+/**
+ * @notest;
+ */
+int LinkedList::getSize() const {
+    return size;
+}
+
+/**
+ * @notest
+ */
+bool LinkedList::isEmpty() const {
+    if (head == nullptr) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-void LinkedList::dump() const {
-    LinkNode* current = head;
-    while (current != nullptr) {
-        std::cout << current->value << " -> ";
-        current = current->next;
-    }
-    std::cout << "nullptr" << std::endl;
+void LinkedList::clear() {
+    throw std::runtime_error("Function not implemented");
 }
+
+void LinkedList::traverse(std::function<void(const int&)> func) const {
+    throw std::runtime_error("Function not implemented");
+}
+
--- tests/test_linkedlist.cpp
+++ tests/test_linkedlist.cpp
@@ -1,55 +1,59 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <stdexcept>
+
 #include "linkedlist.h"
 
-#include <stdexcept>
-#include <iostream>
-#include <cassert>
-
-int main() {
-    // Test LinkNode constructor
-    // Expect: Node creates with value 5
-    LinkNode node(5);
-    assert(node.value == 5);
-
-    // Test LinkedList constructor
-    // Expect: LinkedList creates
+// TEST constructor
+TEST_CASE("Testing constructor") {
     LinkedList llist;
+}
 
-    // Test push_front()
-    // Input: 42
-    // Expect: head = (42, nullptr)
-    llist.push_front(42);
-
-    // Test pop_front()
-    // Expect: llist is empty, return 42
-    assert(llist.pop_front() == 42);
-
-    // Test pop_front()
-    try {
-        llist.pop_front();
-    } catch (const std::runtime_error& e) {
-        std::cout << "Caught excepted exception: " << e.what() << std::endl;
+LinkedList createTestList() {
+    LinkedList llist;
+    for (int i = 0; i < 5; i++) {
+        llist.push_front(i+1);
     }
+    return llist;
+}
 
-    // Test dump()
-    // Except: nullptr
-    llist.dump();
+// TEST get
+TEST_CASE("Testing get()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        CHECK(llist.get(2) == 3);
+    }
+    SUBCASE("exception") {
+        LinkedList llist;
+        CHECK_THROWS_AS(llist.get(-1), std::out_of_range);
+    }
+}
 
-    // Test push_back()
-    // Case 1: Insert into an empty list
-    // Input: 42
-    // Expected: Head is a new node with value 42
+// TEST set
+TEST_CASE("Testing set()") {
+    LinkedList llist = createTestList();
+    llist.set(2, 42);
+    CHECK(llist.get(2) == 42);
+}
+
+// TEST pop_front
+TEST_CASE("Testing pop_front()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        CHECK(llist.pop_front() == 5);
+        CHECK(llist.getSize() == 4);
+    }
+    
+    SUBCASE("exception") {
+        LinkedList llist;
+        CHECK_THROWS_AS(llist.pop_front(), std::runtime_error);
+    }
+}
+
+// TEST push_back
+TEST_CASE("Testing push_back()") {
+    LinkedList llist = createTestList();
     llist.push_back(42);
-    // Case 2: Insert into a nonempty list
-    // Input 43
-    // Expected: Tail is a new node with value 43
-    llist.push_back(43);
-    llist.push_back(44);
-    llist.push_back(45);
-    llist.push_back(46);
-
-    // test dump()
-    // Expect: 42 -> 43 -> 44 -> 45 -> 46 -> nullptr
-    llist.dump();
-
-    std::cout << "All tests passed!" << std::endl;
+    CHECK(llist.get(5) == 42);
+    CHECK(llist.getSize() == 6);
 }
