d4d4cfcfb3988cd24c0f81fcd4861a9f28eccd3f
Shixu Luo <158506816+ShixuLuo@users.noreply.github.com>
2025-01-19 13:37:11 -0500

    chore(release): v0.4.0
    * doc: update changelog
    
    * feat: introduce class LinkedList and class methods
    
    Methods including push_back, push_front, pop_front, get_size, isEmpty.
    
    * feat(linkedlist): implement pop_back method
    
    * doc: update changelog
    
    * feat(linkedlist): introduce insert, remove, find, clear, traverse
    
    * doc: update changelog and readme
    
    * docs: update changelog for version 0.4.0

--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -1,12 +1,19 @@
 #include "linkedlist.h"
 
 #include <stdexcept>
-#include <iostream>
+#include <functional>
 
 LinkNode::LinkNode(int value) : value(value), next(nullptr) {}
 
+/**
+ * @test
+ * @SUBCASE create
+ * INPUT LinkedList llist();
+ * EXPECT llist.head == nullptr;
+ */
 LinkedList::LinkedList() {
     head = nullptr;
+    size = 0;
 }
 
 LinkedList::~LinkedList() {
@@ -19,14 +26,89 @@ LinkedList::~LinkedList() {
     head = nullptr;
 }
 
+// Private
+
+bool LinkedList::isInRange(int index) const {
+    return (index >= 0 && index < size);
+}
+
+void LinkedList::ensureInRange(int index) const {
+    if (!isInRange(index)) {
+        throw std::out_of_range("Out of range");
+    }
+}
+
+LinkNode* LinkedList::getNode(int index) const {
+    // Operation % in C++ is not the same as (mod n).
+    // It might return a negative value if the input is negative.
+    // The implement of a (mod n) is as follow:
+    // (a % n + n) % n;
+    // This will ensure the result is positive.
+    index = (index % size + size) % size;
+    LinkNode* current = head;
+    for (int i = 0; i<index; ++i) {
+        current = current->next;
+    }
+    return current;
+}
+
+// Public
+
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1
+ * INPUT set(2, 42);
+ * EXPECT 5->4->42->2->0
+ */
+void LinkedList::set(int index, int value) {
+    ensureInRange(index);
+
+    LinkNode* node = getNode(index);
+    node->value = value;
+}
+
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1
+ * INPUT llist.get(2);
+ * EXPECT 3;
+ *
+ * CASE 2: exception
+ * INPUT llist.get(-1);
+ * EXPECT std::out_of_range;
+ */
+int LinkedList::get(int index) const {
+    ensureInRange(index);
+
+    LinkNode* node = getNode(index);
+    return node->value;
+}
+
+/**
+ * @notest
+ */
 void LinkedList::push_front(int value) {
     LinkNode* temp = head;
     head = new LinkNode(value);
     head->next = temp;
+    size = size + 1;
 }
 
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1;
+ * INPUT pop_front()
+ * EXPECT 4->3->2->1, size=4;
+ *
+ * CASE 2: exception
+ * INPUT pop_front()
+ * EXPECT std::runtime_error
+ */
 int LinkedList::pop_front() {
-    if (head == nullptr) {
+    if (isEmpty()) {
         throw std::runtime_error("Attempt to delete an element from "
                                  "an empty linked list");
     }
@@ -34,26 +116,206 @@ int LinkedList::pop_front() {
     LinkNode* oldHead = head;
     head = head->next;
     delete oldHead;
+    size = size - 1;
     return value;
 }
 
+/**
+ * @test
+ * CASE 1: normal
+ * SET 5->4->3->2->1;
+ * INPUT push_back(42);
+ * EXPECT 5->4->3->2->1->42, size=6;
+ */
 void LinkedList::push_back(int value) {
-    if (head == nullptr) {
+    if (isEmpty()) {
         head = new LinkNode(value);
     } else {
-        LinkNode* current = head;
-        while (current->next != nullptr) {
-            current = current->next;
-        }
-        current->next = new LinkNode(value);
+        LinkNode* tail = getNode(-1);
+        tail->next = new LinkNode(value);
+    }
+    size = size + 1;
+}
+
+/**
+ * @test
+ * CASE 1: normal;
+ * SET 5->4->3->2->1;
+ * INPUT: pop_back();
+ * EXPECT: 5->4->3->2;
+ * EXPECT: size==4;
+ *
+ * CASE 2: size == 1;
+ * SET 1;
+ * INPUT: pop_back();
+ * EXPECT: nullptr;
+ * EXPECT: size==0;
+ */
+int LinkedList::pop_back() {
+    if (isEmpty()) {
+        throw std::runtime_error("Attempt to delete an element from "
+                            "an empty linked list");
+    }
+    if (size == 1) {
+        return pop_front();
+    }
+    LinkNode* before_tail = getNode(-2);
+    LinkNode* tail = before_tail->next;
+    int value = tail->value;
+    before_tail->next = nullptr;
+    delete tail;
+    size -= 1;
+    return value;
+}
+
+/**
+ * @test
+ * @subcase normal
+ * SET 5->4->3->2->1;
+ * INPUT insert(2, 42);
+ * EXPECT 5->4->42->3->2->1;
+ * EXPECT size == 6;
+ * 
+ * @subcase head
+ * SET 5->4->3->2->1;
+ * INPUT insert(0, 42);
+ * EXPECT 42->5->4->3->2->1;
+ * EXPECT size == 6;
+ */
+void LinkedList::insert(int index, int value) {
+    ensureInRange(index);
+
+    if (index == 0) {
+        push_front(value);
+    } else {
+        LinkNode* node = getNode(index-1);
+        LinkNode* temp = node->next;
+        node->next = new LinkNode(value);
+        node->next->next = temp;
+        size += 1;
     }
 }
 
-void LinkedList::dump() const {
+/**
+ * @test
+ * @subcase normal
+ * SET 5-4-3-2-1;
+ * INPUT remove(2);
+ * EXPECT 5-4-2-1;
+ * EXPECT size == 4;
+ *
+ * @subcase head
+ * SET 5-4-3-2-1;
+ * INPUT remove(0);
+ * EXPECT 4-3-2-1;
+ * EXPECT size == 4;
+ */
+void LinkedList::remove(int index) {
+    ensureInRange(index);
+
+    if (index == 0) {
+        pop_front();
+    } else {
+        LinkNode* node = getNode(index-1);
+        LinkNode* temp = node->next;
+        node->next = temp->next;
+        delete temp;
+        size -= 1;
+    }
+}
+
+/**
+ * @test
+ * @subcase normally found
+ * SET 5-4-3-2-1;
+ * Input find(4);
+ * Expect 1;
+ *
+ * @subcase not found
+ * SET 5-4-3-2-1;
+ * Input find(42);
+ * Expect -1;
+ *
+ * @subcase not found in empty
+ * SET 0;
+ * INPUT find(5);
+ * Expect -1;
+ */
+int LinkedList::find(int value) const {
+   LinkNode* node = head;
+   int index = 0;
+   while (node != nullptr) {
+       if (node->value == value) {
+           return index;
+       } else {
+           node = node->next;
+           index += 1;
+       }
+   }
+   return -1;
+}
+
+/**
+ * @notest;
+ */
+int LinkedList::getSize() const {
+    return size;
+}
+
+/**
+ * @notest
+ */
+bool LinkedList::isEmpty() const {
+    if (head == nullptr && size == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * @test
+ * @subcase normal clear;
+ * SET 5-4-3-2-1;
+ * INPUT clear();
+ * EXPECT isEmpty() == true;
+ * EXPECT size == 0;
+ *
+ * @subcase empty clear;
+ * SET 0
+ * INPUT clear()
+ * EXPECT isEmpty() == true;
+ * EXPECT size == 0;
+ */
+void LinkedList::clear() {
     LinkNode* current = head;
+    LinkNode* temp = current;
     while (current != nullptr) {
-        std::cout << current->value << " -> ";
+        temp = current;
+        current = current->next;
+        delete temp;
+    }
+    head = nullptr;
+    size = 0;
+}
+
+/**
+ * @test
+ * @subcase sum
+ * SET 5-4-3-2-1;
+ * INPUT traverse(sum());
+ * EXPECT 15;
+ *
+ * @subcase print
+ * SET 5-4-3-2-1;
+ * INPUT traverse(print());
+ * EXPECT '5->4->3->2->1->nullptr'
+ */
+void LinkedList::traverse(std::function<void(const int&)> func) const {
+    LinkNode* current = head;
+    for (int i = 0; i < size; ++i) {
+        func(current->value);
         current = current->next;
     }
-    std::cout << "nullptr" << std::endl;
 }
+
--- tests/test_linkedlist.cpp
+++ tests/test_linkedlist.cpp
@@ -1,55 +1,165 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <stdexcept>
+#include <string>
+
 #include "linkedlist.h"
 
-#include <stdexcept>
-#include <iostream>
-#include <cassert>
-
-int main() {
-    // Test LinkNode constructor
-    // Expect: Node creates with value 5
-    LinkNode node(5);
-    assert(node.value == 5);
-
-    // Test LinkedList constructor
-    // Expect: LinkedList creates
+// TEST constructor
+TEST_CASE("Testing constructor") {
     LinkedList llist;
+}
 
-    // Test push_front()
-    // Input: 42
-    // Expect: head = (42, nullptr)
-    llist.push_front(42);
+LinkedList createTestList() {
+    LinkedList llist;
+    for (int i = 0; i < 5; i++) {
+        llist.push_front(i+1);
+    }
+    return llist;
+}
 
-    // Test pop_front()
-    // Expect: llist is empty, return 42
-    assert(llist.pop_front() == 42);
+// TEST get
+TEST_CASE("Testing get()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        CHECK(llist.get(2) == 3);
+    }
+    SUBCASE("exception") {
+        LinkedList llist;
+        CHECK_THROWS_AS(llist.get(-1), std::out_of_range);
+    }
+}
 
-    // Test pop_front()
-    try {
-        llist.pop_front();
-    } catch (const std::runtime_error& e) {
-        std::cout << "Caught excepted exception: " << e.what() << std::endl;
+// TEST set
+TEST_CASE("Testing set()") {
+    LinkedList llist = createTestList();
+    llist.set(2, 42);
+    CHECK(llist.get(2) == 42);
+}
+
+// TEST pop_front
+TEST_CASE("Testing pop_front()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        CHECK(llist.pop_front() == 5);
+        CHECK(llist.getSize() == 4);
+    }
+    
+    SUBCASE("exception") {
+        LinkedList llist;
+        CHECK_THROWS_AS(llist.pop_front(), std::runtime_error);
+    }
+}
+
+// TEST push_back
+TEST_CASE("Testing push_back()") {
+    LinkedList llist = createTestList();
+    llist.push_back(42);
+    CHECK(llist.get(5) == 42);
+    CHECK(llist.getSize() == 6);
+}
+
+// TEST pop_back
+TEST_CASE("Testing pop_back()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        CHECK(llist.pop_back() == 1);
+        CHECK(llist.getSize() == 4);
+    }
+    
+    SUBCASE("size == 1") {
+        LinkedList llist;
+        llist.push_back(1);
+        CHECK(llist.pop_back() == 1);
+        CHECK(llist.getSize() == 0);
+        CHECK(llist.isEmpty());
+    }
+}
+
+// TEST insert
+TEST_CASE("Testing insert()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        llist.insert(2, 42);
+        CHECK(llist.get(2) == 42);
+        CHECK(llist.get(3) == 3);
+        CHECK(llist.getSize() == 6);
     }
 
-    // Test dump()
-    // Except: nullptr
-    llist.dump();
+    SUBCASE("head") {
+        LinkedList llist = createTestList();
+        llist.insert(0, 42);
+        CHECK(llist.get(0) == 42);
+        CHECK(llist.get(1) == 5);
+        CHECK(llist.getSize() == 6);
+    }
+}
 
-    // Test push_back()
-    // Case 1: Insert into an empty list
-    // Input: 42
-    // Expected: Head is a new node with value 42
-    llist.push_back(42);
-    // Case 2: Insert into a nonempty list
-    // Input 43
-    // Expected: Tail is a new node with value 43
-    llist.push_back(43);
-    llist.push_back(44);
-    llist.push_back(45);
-    llist.push_back(46);
+// TEST remove
+TEST_CASE("Testing remove()") {
+    SUBCASE("normal") {
+        LinkedList llist = createTestList();
+        llist.remove(2);
+        CHECK(llist.get(2) == 2);
+        CHECK(llist.getSize() == 4);
+    }
+    SUBCASE("head") {
+        LinkedList llist = createTestList();
+        llist.remove(0);
+        CHECK(llist.get(0) == 4);
+        CHECK(llist.getSize() == 4);
+    }
+}
 
-    // test dump()
-    // Expect: 42 -> 43 -> 44 -> 45 -> 46 -> nullptr
-    llist.dump();
+// TEST find
+TEST_CASE("Testing find()") {
+    SUBCASE("Normally found") {
+        LinkedList llist = createTestList();
+        CHECK(llist.find(4) == 1);
+    }
+    SUBCASE("Normally not found") {
+        LinkedList llist = createTestList();
+        CHECK(llist.find(42) == -1);
+    }
+    SUBCASE("Not found in empty") {
+        LinkedList llist;
+        CHECK(llist.find(42) == -1);
+    }
+}
 
-    std::cout << "All tests passed!" << std::endl;
+// TEST clear
+TEST_CASE("Testing clear()") {
+    SUBCASE("Normal clear") {
+        LinkedList llist = createTestList();
+        llist.clear();
+        CHECK("llist.isEmpty() == true");
+    }
+    SUBCASE("Clear empty") {
+        LinkedList llist;
+        llist.clear();
+        CHECK("llist.isEmpty() == true");
+    }
+}
+
+// TEST traverse
+TEST_CASE("Testing traverse()") {
+    SUBCASE("sum") {
+        LinkedList llist = createTestList();
+        int sum = 0;
+        llist.traverse([&sum](int i) {
+                sum = sum + i;
+                });
+        CHECK(sum == 15);
+    }
+    SUBCASE("print") {
+        LinkedList llist = createTestList();
+        std::string dump = "";
+        std::string expect = "5->4->3->2->1->nullptr";
+        llist.traverse([&dump](int i) {
+                std::string i_s = std::to_string(i);
+                dump = dump + i_s + "->";
+                });
+        dump += "nullptr";
+        CHECK(dump == expect);
+    }
 }
