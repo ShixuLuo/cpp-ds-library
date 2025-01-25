96739373d9697ccb09951964cac394179f217fa4
Shixu Luo <luo.shix@northeastern.edu>
2025-01-25 13:05:09 -0500

    chore: move headers of list into the subfolder list/

--- tests/check_headers.cpp
+++ tests/check_headers.cpp
@@ -2,8 +2,8 @@
  * @file check_header.cpp
  * @brief Collects all headers and checks syntax errors.
  */
-#include "list_interface.h"
-#include "abstract_list.h"
+#include "list/list_interface.h"
+#include "list/abstract_list.h"
 
 int main() {
     return 0;
